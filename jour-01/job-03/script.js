15dce5e35efadfc84e18c9be88b5d41c4845cc4a
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-09-06 16:18:29 +0200

    remove .length

--- jour-01/job-03/script.js
+++ jour-01/job-03/script.js
@@ -1,7 +1,17 @@
+function strLength(str) {
+    count = 0;
+
+    for (char in str) {
+        count++
+    }
+
+    return count
+}
+
 function myIsInString(haystack, needle) {
-    for (let i = 0; i <= haystack.length - needle.length; i++) {
+    for (let i = 0; i <= strLength(haystack) - strLength(needle); i++) {
         let isMatch = true;
-        for (let j = 0; j < needle.length; j++) {
+        for (let j = 0; j < strLength(needle); j++) {
             if (haystack[i + j] !== needle[j]) {
                 isMatch = false;
                 break;
