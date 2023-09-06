112a7f3c9742528b22703b907ce541947221ae98
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-09-06 15:12:36 +0200

    job 02

---
+++ jour-01/job-02/script.js
@@ -0,0 +1,13 @@
+function myCountChar(haystack, needle) {
+    let count = 0;
+
+    for (let char of haystack) {
+        if (char === needle) {
+            count++
+        }
+    }
+
+    return count
+}
+
+console.log(myCountChar('Hello World', 'o'))
