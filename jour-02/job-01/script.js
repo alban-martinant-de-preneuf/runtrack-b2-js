c3eab047cdbd256f0539d7a3b64f87e58b24e01b
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-09-07 15:01:40 +0200

    job 01

---
+++ jour-02/job-01/script.js
@@ -0,0 +1,17 @@
+const html = document.querySelector("html");
+
+function changeColor() {
+    if (window.innerWidth >= 1337) {
+        html.style.backgroundColor = "#ffb703";
+    } else if (window.innerWidth <= 1336 && window.innerWidth > 505) {
+        html.style.backgroundColor = "#d90429";
+    } else {
+        html.style.backgroundColor = "#003049";
+    }
+}
+
+window.addEventListener('resize', () => {
+    changeColor();
+});
+
+changeColor();
