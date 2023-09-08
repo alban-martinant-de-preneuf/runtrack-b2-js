2733a868004564c7028bdd006ed87909d2d4a71b
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-09-08 16:37:47 +0200

    job 04

---
+++ jour-03/class/Board.js
@@ -0,0 +1,89 @@
+export class Board {
+
+    constructor() {
+        this.initializeBoard()
+    }
+
+    initializeBoard() {
+        this.grid = [
+            ['-', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ]
+
+        this.hasWinner = false
+    }
+
+    displayBoard() {
+        const board = document.getElementById('board')
+        board.innerHTML = ""
+        for (let i = 0; i < 3; i++) {
+            const row = document.createElement('div')
+            row.classList.add('row')
+            for (let j = 0; j < 3; j++) {
+                const button = document.createElement('button')
+                button.classList.add('case')
+                button.id = "btn-" + i + "-" + j
+                button.innerHTML = this.grid[i][j]
+                row.appendChild(button)
+            }
+            board.appendChild(row)
+        }
+    }
+
+    placeMove(row, col, symbol) {
+        if (this.grid[row][col] === "-") {
+            this.grid[row][col] = symbol
+            console.log("test")
+            console.log(this.grid[row][col].innerHTML)
+            this.displayBoard()
+            return true
+        }
+        return false
+    }
+
+    checkVictory() {
+        const playerSymbols = ['X', 'O'];
+        for (let playerSymbol of playerSymbols) {
+            // verify rows
+            for (let i = 0; i < 3; i++) {
+                if (this.grid[i][0] === playerSymbol && this.grid[i][1] === playerSymbol && this.grid[i][2] === playerSymbol) {
+                    this.hasWinner = true
+                    return true
+                }
+            }
+            // verify columns
+            for (let i = 0; i < 3; i++) {
+                if (this.grid[0][i] === playerSymbol && this.grid[1][i] === playerSymbol && this.grid[2][i] === playerSymbol) {
+                    this.hasWinner = true
+                    return true
+                }
+            }
+            // verify diagonals
+            const diag1 = this.grid[0][0] === playerSymbol && this.grid[1][1] === playerSymbol && this.grid[2][2] === playerSymbol;
+            const diag2 = this.grid[0][2] === playerSymbol && this.grid[1][1] === playerSymbol && this.grid[2][0] === playerSymbol;
+            if (diag1 || diag2) {
+                this.hasWinner = true
+                return true
+            }
+        }
+        return false
+    }
+
+
+    isfull() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; i < 3; i++) {
+                if (this.grid[i][j] === "-") {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    resetBoard() {
+        this.initializeBoard()
+        this.displayBoard()
+    }
+}
--- jour-03/script.js
+++ jour-03/script.js
@@ -1,1 +1,8 @@
 import { Player } from "./class/Player.js";
+import { Board } from "./class/Board.js";
+
+const p = new Player("X", true);
+const b = new Board();
+
+b.displayBoard();
+b.placeMove(0,0,"X")
